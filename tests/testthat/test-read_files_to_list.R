caabce48d8898cde61aa99695f21557c2233ebd2
AmeliaES <amelia.edmondson-stait@ed.ac.uk>
2025-06-30 09:56:37 +0100

    write tests to expect output is a list of dataframes

--- tests/testthat/test-read_files_to_list.R
+++ tests/testthat/test-read_files_to_list.R
@@ -1,3 +1,18 @@
 test_that("read_files_to_list works", {
-  NULL
+  # Generate tmp directory to test function against
+  tmp_dir <- withr::local_tempdir()
+
+  # Create csv files that match the pattern
+  pattern <- "test_data"
+  test_data_frame <- data.frame(x = 1:5)
+  write.csv(test_data_frame, file.path(tmp_dir, paste0(pattern, "_1.csv")))
+  write.csv(test_data_frame, file.path(tmp_dir, paste0(pattern, "_2.csv")))
+
+  # Expect read_files_to_list returns a list of dataframes
+  output <- read_files_to_list(tmp_dir, pattern)
+  expect_identical(output[[1]][1], test_data_frame)
+  expect_identical(length(output), 2)
+  expect_type(output, "list")
+  expect_s3_class(output[[1]][1], "data.frame")
+
 })
