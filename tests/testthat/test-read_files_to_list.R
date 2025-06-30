12475d1fdb0d3fee5f4629c94560c357675942bb
AmeliaES <amelia.edmondson-stait@ed.ac.uk>
2025-06-30 11:50:17 +0100

    add row.names = FALSE

--- tests/testthat/test-read_files_to_list.R
+++ tests/testthat/test-read_files_to_list.R
@@ -5,8 +5,8 @@ test_that("read_files_to_list works", {
   # Create csv files that match the pattern
   pattern <- "test_data"
   test_data_frame <- data.frame(x = 1:5)
-  write.csv(test_data_frame, file.path(tmp_dir, paste0(pattern, "_1.csv")))
-  write.csv(test_data_frame, file.path(tmp_dir, paste0(pattern, "_2.csv")))
+  write.csv(test_data_frame, file.path(tmp_dir, paste0(pattern, "_1.csv")), row.names = FALSE)
+  write.csv(test_data_frame, file.path(tmp_dir, paste0(pattern, "_2.csv")), row.names = FALSE)
 
   # Expect read_files_to_list returns a list of dataframes
   output <- read_files_to_list(tmp_dir, pattern)
