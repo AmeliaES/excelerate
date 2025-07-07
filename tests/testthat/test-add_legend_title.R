cf9635ffb174f64dbd8d88e9de0f1130c5f4f6a3
Amelia Edmondson-Stait <amelia.edmondson-stait@ed.ac.uk>
2025-07-07 16:51:47 +0100

    begin adding tests for creating readme sheet data

---
+++ tests/testthat/test-add_legend_title.R
@@ -0,0 +1,18 @@
+test_that("add_legend_title adds text into first cell of first sheet", {
+  wb <- createWorkbook()
+  addWorksheet(wb, sheetName = "README")
+  title <- "test title"
+
+  add_legend_title(wb, title)
+
+  # Save to a temporary file
+  temp_file <- tempfile(fileext = ".xlsx")
+  openxlsx::saveWorkbook(wb, temp_file, overwrite = TRUE)
+
+  # Read the first cell of the first sheet
+  cell_value <- openxlsx::read.xlsx(temp_file, sheet = "README", cols = 1, rows = 1, colNames = FALSE)
+
+  # Check if the title was added correctly
+  expect_equal(cell_value[[1,1]], title)
+
+})
---
+++ tests/testthat/test-add_sheet_legend.R
@@ -0,0 +1,36 @@
+test_that("add_sheet_legend works", {
+  # Generate tmp directory to test function against
+  tmp_dir <- withr::local_tempdir()
+
+  # Create csv files that match the pattern
+  pattern <- "test_data"
+  test_data_frame <- data.frame(x = 1:5)
+  test_meta <- data.frame("Col_name" = "X", "description" = "description of the column here")
+  write.csv(test_data_frame, file.path(tmp_dir, paste0(pattern, "_1.csv")), row.names = FALSE)
+  write.csv(test_meta, file.path(tmp_dir, paste0(pattern, "_1.csv.cols")), row.names = FALSE)
+  write.csv(test_data_frame, file.path(tmp_dir, paste0(pattern, "_2.csv")), row.names = FALSE)
+  write.csv(test_meta, file.path(tmp_dir, paste0(pattern, "_2.csv.cols")), row.names = FALSE)
+
+  spreadsheet1 <- spreadsheet(
+    sheet(tmp_dir, paste0(pattern,"_1"), sheet_name = "A", "sheet legend 1"),
+    sheet(tmp_dir, paste0(pattern,"_2"), sheet_name = "B", "sheet legend 2"),
+    title = "Supplementary Table 1",
+    filename = "SuppTab1"
+  )
+
+  wb <- createWorkbook()
+  addWorksheet(wb, sheetName = "README")
+
+  add_sheet_legend(wb, spreadsheet1)
+
+  # Save to a temporary file
+  temp_file <- tempfile(fileext = ".xlsx")
+  openxlsx::saveWorkbook(wb, temp_file, overwrite = TRUE)
+
+  # Read the README sheet
+  README <- openxlsx::read.xlsx(temp_file, sheet = "README", colNames = FALSE)
+
+  # Check if the sheet legends were added
+  expect_equal(README[[1,1]], "sheet legend 1")
+  expect_equal(README[[2,1]], "sheet legend 2")
+})
