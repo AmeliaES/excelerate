62843d68bea84489b4febd8b1d9c7b963f29069f
Amelia Edmondson-Stait <amelia.edmondson-stait@ed.ac.uk>
2025-07-07 17:27:40 +0100

    function to add meta data with sheet name, column name and description under legend text

---
+++ R/add_meta.R
@@ -0,0 +1,20 @@
+#' Function to add column description metadata to README file
+#'
+#' @param wb openxlsx workbook object
+#' @param spreadsheet spreadsheet object returned from spreadsheet()
+#' @param nextFreeRow index of row with empty cell after writing sheet legends
+#'
+#' @importFrom dplyr bind_rows
+#'
+add_meta <- function(wb, spreadsheet, nextFreeRow) {
+
+  # extract the meta data from each results item
+  meta_list <- lapply(spreadsheet$sheets, function(sheet) {
+    sheet$meta
+  })
+
+  # combine all meta data into a single data frame and add id column
+  col_name_descriptions <- bind_rows(meta_list, .id = "sheet_name")
+
+  writeData(wb, sheet = "README", col_name_descriptions, startRow = nextFreeRow, startCol = 1)
+}
