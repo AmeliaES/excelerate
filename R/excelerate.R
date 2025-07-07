ace5ea1bffb1506d8983db72404f85f5a52e986b
Amelia Edmondson-Stait <amelia.edmondson-stait@ed.ac.uk>
2025-07-07 16:51:25 +0100

    start adding function logic to excelerate

--- R/excelerate.R
+++ R/excelerate.R
@@ -1,19 +1,38 @@
+#' Create and save supplementary tables as excel files
+#'
+#' @param ... One or more spreadsheet objects returned from the `spreadsheet()` function.
+#'
+#' @export
+#' @importFrom openxlsx createWorkbook
+#'
 excelerate <- function(...){
   # Check input to function are all of class "spreadsheet"
+  spreadsheets <- list(...)
+
+  if(any(sapply(spreadsheets, class) != "spreadsheet")){
+    stop("Non spreadsheet class used as input to excelerate")
+  }
 
   # For each spreadsheet object create an excel table
+  lapply(spreadsheets, function(spreadsheet){
 
     # Function that creates excel table
+    wb <- createWorkbook()
 
-      # Function that creates README sheet with the following content:
+      # Function that creates README sheet:
+      add_readme(wb, spreadsheet)
 
+      # Insert into README sheet:
         # Function that writes legend title for the excel table (using "title" from spreadsheet())
+        add_legend_title(wb, spreadsheet$title)
 
         # Function that writes the sheet summary (using "sheet_legend" from sheet() )
+        add_sheet_legend(wb, spreadsheet)
 
         # Function that writes column meta data (using "meta" from sheet() )
 
+
       # Function that creates data sheets (using "main" from sheet(), and name of sheet from sheet())
 
-  NULL
+  })
 }
