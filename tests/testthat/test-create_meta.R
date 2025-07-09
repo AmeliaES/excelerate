6923cc7d84e85dd058237bac9bcac2e4b095bf5c
Amelia Edmondson-Stait <amelia.edmondson-stait@ed.ac.uk>
2025-07-09 15:23:00 +0100

    add tests for create_meta

---
+++ tests/testthat/test-create_meta.R
@@ -0,0 +1,24 @@
+test_that("create_meta works", {
+  # Generate tmp directory to test function against
+  tmp_dir <- withr::local_tempdir()
+
+  # Create csv files that match the pattern
+  pattern <- "test_data"
+  test_data_frame <- data.frame(x = 1:5)
+  test_meta <- data.frame("Col_name" = "X", "description" = "description of the column here")
+  write.csv(test_data_frame, file.path(tmp_dir, paste0(pattern, "_1.csv")), row.names = FALSE)
+
+  create_meta(file_name = file.path(tmp_dir, paste0(pattern, "_1.csv")),
+              table_variable_name = test_data_frame,
+              colname_descriptions = c("x" = "description of the column here"))
+
+  # Check if the .cols file was created
+  cols_file <- file.path(tmp_dir, paste0(pattern, "_1.csv.cols"))
+  expect_true(file.exists(cols_file))
+  # Check if the .cols file contains the expected content
+  cols_content <- read.csv(cols_file, stringsAsFactors = FALSE)
+  expect_equal(nrow(cols_content), 1)
+  expect_equal(cols_content$column, "x")
+  expect_equal(cols_content$description, "description of the column here")
+
+})
