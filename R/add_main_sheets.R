3f9250ac3770c141e9230e9c2a7f9122fe2f8735
Amelia Edmondson-Stait <amelia.edmondson-stait@ed.ac.uk>
2025-07-15 13:24:46 +0100

    add autofitting column width to main results sheets

--- R/add_main_sheets.R
+++ R/add_main_sheets.R
@@ -11,11 +11,21 @@ add_main_sheets <- function(wb, spreadsheet) {
   # Add each table to a new sheet
   for (i in seq_along(spreadsheet$sheets)) {
     openxlsx::addWorksheet(wb, sheetName = names(spreadsheet$sheets)[i])
+
     openxlsx::writeData(wb,
       sheet = names(spreadsheet$sheets)[i],
       spreadsheet$sheets[[i]]$results,
       startRow = 1,
       colNames = TRUE
+    )
+
+    # Autofit the widths of all the columns in the results dataframe
+    n_cols <- ncol(spreadsheet$sheets[[i]]$results)
+
+    openxlsx::setColWidths(wb,
+      sheet = names(spreadsheet$sheets)[i],
+      cols = c(1:n_cols),
+      widths = "auto"
     )
   }
 }
