8fd1ecdd7835e09efd91a743178134b25e08736d
Amelia Edmondson-Stait <amelia.edmondson-stait@ed.ac.uk>
2025-07-15 13:56:35 +0100

    add tests for README sheet

--- R/autofit_cols.R
+++ R/autofit_cols.R
@@ -19,8 +19,9 @@ autofit_cols <- function(wb, data, sheet_name) {
   # add a small number to that to add a bit of extra spacing
   extra_char_n <- 4
   for (col in seq_len(ncol(data))) {
-    col_max_width <- max(nchar(as.character(data[[col]])), na.rm = TRUE)
-    +extra_char_n
+    col_max_width <- max(nchar(as.character(data[[col]])),
+      na.rm = TRUE
+    ) + extra_char_n
     openxlsx::setColWidths(wb,
       sheet = sheet_name, cols = col,
       widths = col_max_width
---
+++ tests/testthat/test-autofit_cols.R
@@ -0,0 +1,32 @@
+test_that("the width of the column is max n chars in each col + extra_char_n", {
+  # Use iris dataset
+  data <- iris
+
+  # Create a workbook and add a worksheet
+  wb_expected <- createWorkbook()
+  sheet_name <- "Sheet1"
+  addWorksheet(wb_expected, sheet_name)
+  writeData(wb_expected, sheet_name, data)
+
+  # Set expected widths manually
+  extra_char_n <- 4
+  expected_widths <- sapply(data, function(column) {
+    max(nchar(as.character(column))) + extra_char_n
+  })
+
+  for (col in seq_len(ncol(data))) {
+    setColWidths(wb_expected,
+      sheet = sheet_name, cols = col,
+      widths = expected_widths[col]
+    )
+  }
+
+  # Create another workbook and apply autofit
+  wb_actual <- createWorkbook()
+  addWorksheet(wb_actual, sheet_name)
+  writeData(wb_actual, sheet_name, data)
+  autofit_cols(wb_actual, data, sheet_name)
+
+  # Compare the files
+  expect_equal(wb_actual, wb_expected)
+})
