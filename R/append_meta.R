09dc7d762a76c44a4e64f3c80142c51ade0ca7e5
Amelia Edmondson-Stait <amelia.edmondson-stait@ed.ac.uk>
2025-07-16 15:01:19 +0100

    append_meta also uses iris in example

--- R/append_meta.R
+++ R/append_meta.R
@@ -13,22 +13,19 @@
 #' @importFrom utils write.csv
 #' @examples
 #'
-#' append_meta(
-#'   results = mtcars,
+#' results <- append_meta(
+#'   results = iris,
 #'   colname_descriptions = c(
-#'     "mpg" = "Miles/(US) gallon",
-#'     "cyl" = "Number of cylinders",
-#'     "disp" = "Displacement (cu.in.)",
-#'     "hp" = "Gross horsepower",
-#'     "drat" = "Rear axle ratio",
-#'     "wt" = "Weight (1000 lbs)",
-#'     "qsec" = "1/4 mile time",
-#'     "vs" = "Engine (0 = V-shaped, 1 = straight)",
-#'     "am" = "Transmission (0 = automatic, 1 = manual)",
-#'     "gear" = "Number of forward gears",
-#'     "carb" = "Number of carburetors"
+#'     "Sepal.Length" = "Length of the sepal in cm",
+#'     "Sepal.Width" = "Width of the sepal in cm",
+#'     "Petal.Length" = "Length of the petal in cm",
+#'     "Petal.Width" = "Width of the petal in cm",
+#'     "Species" = "Species of iris"
 #'   )
 #' )
+#'
+#' # Example to show column descriptions are stored as comments
+#' comment(results$Sepal.Length)
 #'
 #' @export
 append_meta <- function(results, colname_descriptions) {
