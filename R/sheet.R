21300fa460c09f241cf3c792964c2d70ec81496e
Amelia Edmondson-Stait <amelia.edmondson-stait@ed.ac.uk>
2025-07-16 12:52:40 +0100

    use iris not mtcars as mtcars has data in rownames which are not saved in excel file, test now catches this

--- R/sheet.R
+++ R/sheet.R
@@ -14,19 +14,13 @@
 #' @examples
 #'
 #' results <- append_meta(
-#'   results = mtcars,
+#'   results = iris,
 #'   colname_descriptions = c(
-#'     "mpg" = "Miles/(US) gallon",
-#'     "cyl" = "Number of cylinders",
-#'     "disp" = "Displacement (cu.in.)",
-#'     "hp" = "Gross horsepower",
-#'     "drat" = "Rear axle ratio",
-#'     "wt" = "Weight (1000 lbs)",
-#'     "qsec" = "1/4 mile time",
-#'     "vs" = "Engine (0 = V-shaped, 1 = straight)",
-#'     "am" = "Transmission (0 = automatic, 1 = manual)",
-#'     "gear" = "Number of forward gears",
-#'     "carb" = "Number of carburetors"
+#'     "Sepal.Length" = "Length of the sepal in cm",
+#'     "Sepal.Width" = "Width of the sepal in cm",
+#'     "Petal.Length" = "Length of the petal in cm",
+#'     "Petal.Width" = "Width of the petal in cm",
+#'     "Species" = "Species of iris"
 #'   )
 #' )
 #' sheet(results, "Example Sheet", "An example legend")
